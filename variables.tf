6f0495511b89b209399ac99a6cf7cb6bcde23f68
Gareth Jones <gareth.jones@ans.co.uk>
2022-07-19 13:37:18 +0100

    inital tf files

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "zone" {
+  type = map(object({
+    zone             = string
+    dns              = string
+    ssl              = string
+    always_use_https = string
+    min_tls_version  = string
+    plan             = string
+    data_records = map(object({
+      name = string
+      data = object({
+        service   = string
+        proto     = string
+        name      = string
+        priority  = number
+        weight    = number
+        port      = number
+        target    = string
+      })
+      type     = string
+      proxied  = bool
+      priority = number
+    }))
+    value_records = map(object({
+      name     = string
+      value    = string
+      type     = string
+      proxied  = bool
+      priority = number
+    }))
+  }))
+}
